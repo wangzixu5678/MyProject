8dc913a5eadb5d3473ce7069015a7659e1dbf9e9
wangzixu03 <wangzixu03@58.com>
2024-08-07 14:58:03 +0800

    33

--- app/src/main/java/com/duodian/myapplication/MainActivity3.kt
+++ app/src/main/java/com/duodian/myapplication/MainActivity3.kt
@@ -7,6 +7,8 @@ class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
-
+        //22
+        //33
+        //443
     }
 }
