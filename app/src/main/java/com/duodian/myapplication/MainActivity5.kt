2e69014df6c2815a89296dc42b07dfb984213f24
wangzixu001089 <wangzixu@hungrystudio.com>
2024-12-24 20:01:49 +0800

    1

--- app/src/main/java/com/duodian/myapplication/MainActivity5.kt
+++ app/src/main/java/com/duodian/myapplication/MainActivity5.kt
@@ -17,7 +17,7 @@ class MainActivity5 : AppCompatActivity() {
     private val mHandler = Handler()
 
 
-    //哈哈3
+    //1
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
